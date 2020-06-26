cf5c3e00e0fa74f76e9e92fb374c42bdf4151de1
paulo bebeto <p.roberto.rodrigues@gmai.com>
2020-06-26 00:35:57 -0300

    mais um

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ProjectShcool_Api.Controllers
@@ -12,16 +13,79 @@ namespace ProjectShcool_Api.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(new List<String> {"a", "b" , "c"}.ToArray());
+            try
+            {
+                return Ok(new List<String> { "a", "b", "c" }.ToArray());
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados falhou");
+            }
         }
 
         [HttpGet("{filt}")]
         public IActionResult Get(string filt)
         {
-            return Ok((new List<String> {"a", "b" , "c"}).Where(x => x == filt));
+            try
+            {
+                return Ok((new List<String> { "a", "b", "c" }).Where(x => x == filt));
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados falhou");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(string filt)
+        {
+            try
+            {
+                return Ok((new List<String> { "a", "b", "c" }).Where(x => x == "filt"));
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados falhou");
+            }
+        }
+
+
+
+
+        [HttpPut("{professorId}")]
+        public IActionResult Put(int professorId)
+        {
+            try
+            {
+                return Ok((new List<String> { "a", "b", "c" }).Where(x => x == "as"));
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados falhou");
+            }
+        }
+
+
+
+        [HttpDelete("{professorId}")]
+        public IActionResult HttpDelete(int professorId)
+        {
+            try
+            {
+                return Ok((new List<String> { "a", "b", "c" }).Where(x => x == ""));
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados falhou");
+            }
         }
 
 
     }
+
+
+
+}
+
 
 }
