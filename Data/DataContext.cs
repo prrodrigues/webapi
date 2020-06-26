3201b84b877f3a2010054f7a16bf90d15276d23d
paulo bebeto <p.roberto.rodrigues@gmai.com>
2020-06-26 00:16:03 -0300

    Commit

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,11 +1,69 @@
- using System;
+using System;
+using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-
+using ProjectShcool_Api.Models;
 
 namespace ProjectShcool_Api.Data
 {
     public class DataContext : DbContext
     {
-        
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Professor> Professores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder){
+            builder.Entity<Professor>()
+            .HasData(
+                new List<Professor>(){
+                    new Professor() {
+                    id = 1,
+                    nome = "Vinicius",
+                    sobrenome = "de Moraes",
+                    dataNasc = "01-02-2000"
+                    },
+                    new Professor() {
+                    id = 2,
+                    nome = "Helena",
+                    sobrenome = "Gonzales",
+                    dataNasc = "01-01-1999"
+                    },
+                    new Professor() {
+                    id = 3,
+                    nome = "Luna",
+                    sobrenome = "de Lunares",
+                    dataNasc = "01-01-1800"
+                    },
+                }
+            );
+
+            builder.Entity<Aluno>()
+            .HasData(
+                new List<Aluno>(){
+                    new Aluno() {
+                    id = 1,
+                    nome = "Aluno Vinicius",
+                    sobrenome = "de Moraes",
+                    dataNasc = "01-02-2010",
+                    professorId = 2
+                    },
+                    new Aluno() {
+                    id = 2,
+                    nome = "Aluno Helena",
+                    sobrenome = "Gonzales",
+                    dataNasc = "01-01-2005",
+                    professorId = 3
+                    },
+                    new Aluno() {
+                    id = 3,
+                    nome = "Aluno Luna",
+                    sobrenome = "de Lunares",
+                    dataNasc = "01-01-2012",
+                    professorId = 1
+                    },
+                }
+            );
+
+
+        }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ProjectShcool_Api.Data;
 
 namespace ProjectShcool_Api
 {
@@ -25,6 +27,9 @@ namespace ProjectShcool_Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(x => x.UseSqlite(
+                Configuration.GetConnectionString("DefaultConnection")
+            ));
             services.AddControllers();
         }
 
